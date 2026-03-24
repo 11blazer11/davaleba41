4591c7e47f3804585b3b8dbc9f59d7f82ce2a598
unknown <bebiashvili.dachi@gmail.com>
2026-03-24 12:20:58 +0400

    davaleba41

---
+++ App/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+#authenticate, login, logout gamoyeneba chatgpt davixmare
+#kid argon2 maq gamoyenebuli
+
+def register_view(request):
+    if request.method == 'POST':
+        base_username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        username = base_username
+        counter = 1
+        while User.objects.filter(username=username).exists():
+            username = f"{base_username}{counter}"
+            counter += 1
+
+        User.objects.create_user(username=username, password=password)
+        messages.success(request, f"Account created successfully as '{username}'")
+        return redirect('login')
+
+    return render(request, 'register.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(
+            request,
+            username=request.POST.get('username'),
+            password=request.POST.get('password')
+        )
+
+        if user:
+            login(request, user)
+            return redirect('main') 
+        else:
+            messages.error(request, "ver shexvedi")
+
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
+def main_view(request):
+    return render(request, 'main_page.html')
