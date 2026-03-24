83e1d4f34f50e7b5cbedf86282148e3aedb27bef
unknown <bebiashvili.dachi@gmail.com>
2026-03-24 16:51:23 +0400

    usercreationform

--- App/views.py
+++ App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -8,20 +9,18 @@ from django.contrib import messages
 #kide argon2 maq gamoyenebuli
 
 def register_view(request):
-    if request.method == 'POST':
-        base_username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            return redirect('login')
+        
+    else:
+        form = UserCreationForm()
 
-        username = base_username
-        counter = 1
-        while User.objects.filter(username=username).exists():
-            username = f"{base_username}{counter}"
-            counter += 1
+    return render(request, 'register.html', {'form': form})
 
-        User.objects.create_user(username=username, password=password)
-        return redirect('login')
 
-    return render(request, 'register.html')
 
 
 def login_view(request):
